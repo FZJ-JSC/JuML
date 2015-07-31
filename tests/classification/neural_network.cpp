ae3c9459efc919718579f4554bddb2ea09a845b1
cbodenst <cbodenstein@gmx.de>
2015-07-31 11:03:50 +0200

    Improve template class and remove sample classes and tests

--- tests/classification/neural_network.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-TEST (SampleTest, SampleCase){
-  EXPECT_EQ(42,42);
-}
-
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-return RUN_ALL_TESTS();
-}
