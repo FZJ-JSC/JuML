f695570d6ff0092274ca8cbc50670882af3a2285
Philipp Glock <phil.glock@gmail.com>
2016-01-12 16:44:28 +0100

    BaseClassifier and GaussianNaiveBayes signatures changed to use arrayfire

--- include/classification/GaussianNaiveBayes.h
+++ include/classification/GaussianNaiveBayes.h
@@ -9,7 +9,7 @@
 * Description: Header of class GaussianNaiveBayes
 *
 * Maintainer: p.glock
-* 
+*
 * Email: phil.glock@gmail.com
 */
 
@@ -25,31 +25,30 @@
 namespace juml {
     class GaussianNaiveBayes : public BaseClassifier {
     protected:
-        arma::Col<float> class_counts_;
-        arma::Col<float> prior_;
-        arma::Mat<float> theta_;
-        arma::Mat<float> stddev_;
+        af::array class_counts_;
+        af::array prior_;
+        af::array theta_;
+        af::array stddev_;
 
     public:
-        void fit(Dataset<float>& X, Dataset<int>& y);
-        Dataset<int> predict(const Dataset<float>& X) const;
-        Dataset<float> predict_probability(const Dataset<float>& X) const;
-        float accuracy(const Dataset<float>& X, const Dataset<int>& y) const;
+        void fit(Dataset& X, Dataset& y);
+        Dataset predict(const Dataset& X) const;
+        Dataset predict_probability(const Dataset& X) const;
+        float accuracy(const Dataset& X, const Dataset& y) const;
 
-        inline const arma::Col<float>& class_counts() const {
+        inline const af::array& class_counts() const {
             return this->class_counts_;
         };
-        inline const arma::Col<float>& prior() const {
+        inline const af::array& prior() const {
             return this->prior_;
         };
-        inline const arma::Mat<float>& theta() const {
+        inline const af::array& theta() const {
             return this->theta_;
         };
-        inline const arma::Mat<float>& stddev() const {
+        inline const af::array& stddev() const {
             return this->stddev_;
         };
     };
 } // namespace juml
 
 #endif // GAUSSIANNAIVEBAYES_H
-
