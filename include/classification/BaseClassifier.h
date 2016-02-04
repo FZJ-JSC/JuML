174dfdb99063f61fac7cd3006bb595ed89ddbdc5
Markus Goetz <murxman@gmail.com>
2016-02-04 14:50:09 +0100

    Added BaseClassifier, factored abstract methods out to a .cpp file

--- include/classification/BaseClassifier.h
+++ include/classification/BaseClassifier.h
@@ -16,33 +16,29 @@
 #ifndef BASECLASSIFIER_H
 #define BASECLASSIFIER_H
 
-#include <armadillo>
 #include <mpi.h>
 
 #include "data/Dataset.h"
 #include "preprocessing/ClassNormalizer.h"
+#include "util/Definitions.h"
 
 namespace juml {
     class BaseClassifier {
     protected:
-        MPI_Comm comm_;
+        Backend backend_;
+        
         ClassNormalizer class_normalizer_;
-        int mpi_size_;
+        
+        MPI_Comm comm_;
         int mpi_rank_;
+        int mpi_size_;
 
     public:
-        BaseClassifier(MPI_Comm comm=MPI_COMM_WORLD) 
-            : comm_(comm), class_normalizer_(comm) {
-            MPI_Comm_size(this->comm_, &this->mpi_size_);
-            MPI_Comm_rank(this->comm_, &this->mpi_rank_);
-        };
+        BaseClassifier(Backend backend, MPI_Comm comm=MPI_COMM_WORLD);
 
-        virtual inline void fit(Dataset<float>& X, Dataset<int>& y) {
-            this->class_normalizer_.index(y);
-        };
-
-        virtual Dataset<int> predict(const Dataset<float>& X) const = 0;
-        virtual float accuracy(const Dataset<float>& X, const Dataset<int>& y) const = 0;
+        virtual void fit(Dataset& X, Dataset& y);
+        virtual Dataset predict(const Dataset& X) const = 0;
+        virtual double accuracy(const Dataset& X, const Dataset& y) const = 0;
     };
 } // namespace juml
 
