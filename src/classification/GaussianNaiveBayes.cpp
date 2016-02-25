94a2e9ab7cc43ce885136dd0536204a249f2c86e
Philipp Glock <phil.glock@gmail.com>
2016-02-25 09:46:02 +0100

    call load_equal_chunks on X and y in predict_probability and accuracy

--- src/classification/GaussianNaiveBayes.cpp
+++ src/classification/GaussianNaiveBayes.cpp
@@ -104,9 +104,10 @@ namespace juml {
         delete[] message;
     }
 
-    Dataset GaussianNaiveBayes::predict_probability(const Dataset& X) const {
+    Dataset GaussianNaiveBayes::predict_probability(Dataset& X) const {
         const dim_t n_classes = this->class_normalizer_.n_classes();
         af::setBackend(static_cast<af::Backend>(this->backend_.get()));
+        X.load_equal_chunks();
         af::array probabilities = af::constant(1.0f, n_classes, X.n_samples());
         
         const af::array& X_ = X.data();
@@ -125,7 +126,8 @@ namespace juml {
         return Dataset(probabilities, this->comm_);
     }
 
-    Dataset GaussianNaiveBayes::predict(const Dataset& X) const {
+    Dataset GaussianNaiveBayes::predict(Dataset& X) const {
+        // X is loaded in this->predict_probability
         Dataset probabilities = this->predict_probability(X);
         
         af::array values(X.n_samples());
@@ -137,10 +139,12 @@ namespace juml {
         return Dataset(locations_orig, this->comm_);
     }
 
-    float GaussianNaiveBayes::accuracy(const Dataset& X, const Dataset& y) const {
+    float GaussianNaiveBayes::accuracy(Dataset& X, Dataset& y) const {
         float local_results[2];
         Dataset predictions = this->predict(X);
-        
+        // X is loaded in this->predict
+        y.load_equal_chunks();
+
         af::array sum = af::sum(predictions.data() == y.data());
         local_results[0] = (float)sum.scalar<uint>();
         local_results[1] = (float)y.n_samples();
