af873d5d0c3a3b6cc931b5d04930ac45bc2772b4
mricherzhagen <mricherzhagen@users.noreply.github.com>
2016-03-21 14:09:51 +0100

    move ANN-Activation functions to separate header file

--- include/classification/ANNLayers.h
+++ include/classification/ANNLayers.h
@@ -1,9 +1,27 @@
+/*
+* Copyright (c) 2015
+* Forschungszentrum Juelich GmbH, Juelich Supercomputing Center
+*
+* This software may be modified and distributed under the terms of BSD-style license.
+*
+* File name: ANNLayers.h
+*
+* Description: Header File that describes the Layers used for Artifical Neural Networks
+*
+* Maintainer: 
+*
+* Email: 
+*/
+
+
+
 #ifndef JUML_ANNLAYERS_H_
 #define JUML_ANNLAYERS_H_
 #include<arrayfire.h>
 #include<iostream>
 #include<mpi.h>
 #include "core/MPI.h"
+#include "classification/ANNActivations.h"
 namespace juml {
 	namespace ann {
 		class Layer {
@@ -99,49 +117,7 @@ namespace juml {
 
 		typedef std::shared_ptr<Layer> LayerPtr;
 
-		enum class Activation { Sigmoid, TanH, Linear };
 
-		template<Activation T>
-		inline af::array activation(const af::array& in) {
-			throw std::runtime_error("Not implemented");
-		}
-
-		template<Activation T>
-		inline af::array activation_deriv(const af::array& out) {
-			throw std::runtime_error("Not implemented");
-		}
-//Linear:
-		template<>
-		inline af::array activation<Activation::Linear>(const af::array& in) {
-			return in;
-		}
-
-		template<>
-		inline af::array activation_deriv<Activation::Linear>(const af::array& out) {
-			return af::constant(1.0f, out.dims(0), out.dims(1));
-		}
-
-//Sigmoid:
-		template<>
-		inline af::array activation<Activation::Sigmoid>(const af::array& in) {
-			return af::sigmoid(in);
-		}
-		template<>
-		inline af::array activation_deriv<Activation::Sigmoid>(const af::array &out) {
-			return out * (1 - out);
-		}
-//TanH
-		template<>
-		inline af::array activation<Activation::TanH>(const af::array& in) {
-			return af::tanh(in);
-		}
-
-		template<>
-		inline af::array activation_deriv<Activation::TanH>(const af::array &out) {
-			return (1 - out * out);
-		}
-
-		//TODO implement a template-'FunctionLayer' with template-spezialisations for different activation functions?
 		template<Activation T>
 		class FunctionLayer: public Layer {
 			public: 
