81dce0d463a0c349b7f4faa5ace7f449e601583a
mricherzhagen <mricherzhagen@users.noreply.github.com>
2016-04-04 10:23:33 +0200

    Added time measurements to ann-mnist executable

--- executables/ann-mnist/ann-mnist.cpp
+++ executables/ann-mnist/ann-mnist.cpp
@@ -7,6 +7,9 @@ int main(int argc, char *argv[]) {
 	using std::cout;
 	using std::endl;
 	MPI_Init(&argc, &argv);
+
+	double time_start = MPI_Wtime();
+
 	int mpi_size, mpi_rank;
 	MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
@@ -27,6 +30,8 @@ int main(int argc, char *argv[]) {
 	}
 	cout << "Seed: " << af::getSeed() << endl;
 
+	double time_init_af = MPI_Wtime();
+
 	juml::SequentialNeuralNet net(backend);
 	//net.add(juml::ann::make_SigmoidLayer(28*28, 30));
 	//net.add(juml::ann::make_SigmoidLayer(30, 10));
@@ -34,7 +39,9 @@ int main(int argc, char *argv[]) {
 	net.add(juml::ann::make_SigmoidLayer(28*28, 100));
 	net.add(juml::ann::make_SigmoidLayer(100, 50));
 	net.add(juml::ann::make_SigmoidLayer(50, 10));
-	//net.save("mnist-net.h5", false);
+	//net.save("mnist-net.h5", false);a
+	
+	double time_init_net = MPI_Wtime();	
 
 	//70000 x 28 x 28 will be read as 28 x 28 x 70000?
 	juml::Dataset data(argv[1], "Data");
@@ -44,6 +51,8 @@ int main(int argc, char *argv[]) {
 	data.load_equal_chunks();
 	label.load_equal_chunks();
 
+	double time_loaded_data = MPI_Wtime();
+
 
 	const int dataset_stepsize = 11;
 	af::array data_array = data.data();
@@ -52,11 +61,11 @@ int main(int argc, char *argv[]) {
 	af::array full_data = data_array;
 
 	//data_array = data_array(af::span, af::seq(0, data_array.dims(1) - 1, dataset_stepsize));
-	data_array = data_array(af::span, af::seq(0, 8000 - 1));
+	data_array = data_array(af::span, af::seq(0, 8000/mpi_size - 1));
 
 	af::array label_array = label.data();
 	//label_array = label_array(af::span, af::seq(0, label_array.dims(1) - 1, dataset_stepsize));
-	label_array = label_array(af::span, af::seq(0, 8000 - 1));
+	label_array = label_array(af::span, af::seq(0, 8000/mpi_size - 1));
 
 	const int N = data_array.dims(1);
 	int globalN;
@@ -65,8 +74,11 @@ int main(int argc, char *argv[]) {
 	af::array target = af::constant(0, 10, N, s32);
 	target(af::range(N).T() *  10 + label_array)= 1;
 
+	double time_splitted_data = MPI_Wtime();
+	double time_train_batch_test = 0;
+	double time_train_sync = 0;
 
-	int batchsize = 400;
+	int batchsize = 10;
 	int nbatches = N/batchsize;
 	cout << "N: " << N << " n_batches: " << nbatches << endl
 		<< "batchsize: " << batchsize <<endl;
@@ -80,21 +92,44 @@ int main(int argc, char *argv[]) {
 			lasterror = net.fitBatch(batchsamples, batchtarget, 2);
 			error += lasterror;
 		}
+		double time_buf = MPI_Wtime();
 		net.sync();
+		time_train_sync += MPI_Wtime() - time_buf;
+
+		time_buf = MPI_Wtime();
 
 		cout << " Epoch " << epoch << " Error: " << error / nbatches 
 			<< " Last: " << lasterror << endl;
 		cout << "Train-Class-Accuracy: " << (net.classify_accuracy_array(data_array, label_array) / (float) globalN) << endl;
+		time_train_batch_test += MPI_Wtime() - time_buf;
 		if (error / nbatches < 0.25) {
 			break;
 		}
 	}
+
+	double time_trained = MPI_Wtime();
 	
 	juml::Dataset full_data_set(full_data);
 	cout << "Full Class-Accuracy: " << net.classify_accuracy(full_data_set, label) << endl;
+
+	double time_tested = MPI_Wtime();
 	/*juml::Dataset trainset(data_array);
 	juml::Dataset labelset(target);
 	net.fit(trainset, labelset);*/
+
+	cout << "Time Measuerment: " << endl;
+	#define Fs(time, name) printf("%20s %3.4f\n", name, time);
+	#define F(start,end,name) Fs(end - start, name);
+
+	F(time_start, time_init_af, "AF-init");	
+	F(time_init_af, time_init_net, "Net-init");
+	F(time_init_net, time_loaded_data, "Data load");
+	F(time_loaded_data, time_splitted_data, "Split data");
+	F(time_splitted_data, time_trained, "Training");
+	Fs(time_train_batch_test, "Training-Test");
+	Fs(time_train_sync, "Training-Sync");
+	F(time_trained, time_tested, "Testing");
+	F(time_start, time_tested, "Total");
 
 	MPI_Finalize();
 }
