08fddf5715b8b45d77a48056975e9d9af3929d92
Philipp Glock <phil.glock@gmail.com>
2016-04-05 08:20:12 +0200

    Added documentation to Algorithm

--- include/core/Algorithm.h
+++ include/core/Algorithm.h
@@ -21,17 +21,55 @@
 #include <mpi.h>
 
 namespace juml {
-    //! Algorithm
-    //! TODO: Describe me
+    /**
+     * Algorithm
+     *
+     * Abstract class, that is subclassed by all algorithms. It provides a basic interface.
+     */
     class Algorithm {
     protected:
+        /**
+         * @var backend_
+         * @brief execution backend. Holds the backend on which the algorithm is running. (@see Backend)
+         */
         Backend backend_;
-        
+
+        /**
+         * @var comm_
+         * @brief mpi communicator.
+         */
         MPI_Comm comm_;
+
+        /**
+         * @var mpi_rank_
+         * @brief mpi rank of the process. An integer with the processes rank. Different for each mpi process.
+         */
         int mpi_rank_;
+
+        /**
+         * @var mpi_size_
+         * @brief mpi size of the communicator. Number of processes in the communicator. Same for each mpi process.
+         */
         int mpi_size_;
     public:
+        /**
+         * Algorithm constructor
+         * @param backend The execution backend, defaults to CPU. (@see Backend)
+         * @param comm The MPI communicator for the execution, defaults to world communicator.
+         */
         Algorithm(int backend=Backend::CPU, MPI_Comm comm=MPI_COMM_WORLD);
+
+        /**
+         * Abstract interface to load an algorithm from a hdf5 file.
+         * @param filename The filename of the loaded file.
+         */
+        void load(const std::string& filename) = 0;
+
+        /**
+         * Abstract interface to save an algorithm to a hdf5 file.
+         * @param filename The filename of the saved file.
+         */
+        void save(const std::string& filename) = 0;
     }; // Algorithm
 }  // juml
 #endif // ALGORITHM_H
